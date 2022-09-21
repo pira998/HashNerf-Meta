99c238dcb88e1ed5e387f5433778f96e549f603b
Praveen <piraveen.18@cse.mrt.ac.lk>
2022-09-21 18:19:39 +0530

    add wandb to test file

--- shapenet_test.py
+++ shapenet_test.py
@@ -8,6 +8,7 @@ from datasets.shapenet import build_shapenet
 from models.nerf import build_nerf
 from utils.shape_video import create_360_video
 from models.rendering import get_rays_shapenet, sample_points, volume_render
+import wandb
 
 
 def test_time_optimize(args, model, optim, imgs, poses, hwf, bound):
@@ -23,14 +24,15 @@ def test_time_optimize(args, model, optim, imgs, poses, hwf, bound):
     for step in range(args.tto_steps):
         indices = torch.randint(num_rays, size=[args.tto_batchsize])
         raybatch_o, raybatch_d = rays_o[indices], rays_d[indices]
-        pixelbatch = pixels[indices] 
+        pixelbatch = pixels[indices]
         t_vals, xyz = sample_points(raybatch_o, raybatch_d, bound[0], bound[1],
                                     args.num_samples, perturb=True)
-        
+
         optim.zero_grad()
         rgbs, sigmas = model(xyz)
         colors = volume_render(rgbs, sigmas, t_vals, white_bkgd=True)
         loss = F.mse_loss(colors, pixelbatch)
+        wandb.log({'tto_loss': loss})
         loss.backward()
         optim.step()
 
@@ -46,7 +48,7 @@ def report_result(args, model, imgs, poses, hwf, bound):
         rays_o, rays_d = rays_o.reshape(-1, 3), rays_d.reshape(-1, 3)
         t_vals, xyz = sample_points(rays_o, rays_d, bound[0], bound[1],
                                     args.num_samples, perturb=False)
-        
+
         synth = []
         num_rays = rays_d.shape[0]
         with torch.no_grad():
@@ -60,18 +62,22 @@ def report_result(args, model, imgs, poses, hwf, bound):
             error = F.mse_loss(img, synth)
             psnr = -10*torch.log10(error)
             view_psnrs.append(psnr)
-    
+
     scene_psnr = torch.stack(view_psnrs).mean()
+    wandb.log({'test/scene_psnr': scene_psnr})
     return scene_psnr
 
 
 def test():
-    parser = argparse.ArgumentParser(description='shapenet few-shot view synthesis')
+    parser = argparse.ArgumentParser(
+        description='shapenet few-shot view synthesis')
     parser.add_argument('--config', type=str, required=True,
-                    help='config file for the shape class (cars, chairs or lamps)')    
+                        help='config file for the shape class (cars, chairs or lamps)')
     parser.add_argument('--weight-path', type=str, required=True,
                         help='path to the meta-trained weight file')
+    parser.add_argument('--model_type', type=str, default='SimpleNeRF')
     args = parser.parse_args()
+    wandb.init(project='nerf-meta', config=args)
 
     with open(args.config) as config:
         info = json.load(config)
@@ -81,8 +87,8 @@ def test():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     test_set = build_shapenet(image_set="test", dataset_root=args.dataset_root,
-                            splits_path=args.splits_path,
-                            num_views=args.tto_views+args.test_views)
+                              splits_path=args.splits_path,
+                              num_views=args.tto_views+args.test_views)
     test_loader = DataLoader(test_set, batch_size=1, shuffle=False)
 
     model = build_nerf(args)
@@ -93,29 +99,35 @@ def test():
 
     savedir = Path(args.savedir)
     savedir.mkdir(exist_ok=True)
-    
+
     test_psnrs = []
     for idx, (imgs, poses, hwf, bound) in enumerate(test_loader):
-        imgs, poses, hwf, bound = imgs.to(device), poses.to(device), hwf.to(device), bound.to(device)
-        imgs, poses, hwf, bound = imgs.squeeze(), poses.squeeze(), hwf.squeeze(), bound.squeeze()
+        imgs, poses, hwf, bound = imgs.to(device), poses.to(
+            device), hwf.to(device), bound.to(device)
+        imgs, poses, hwf, bound = imgs.squeeze(
+        ), poses.squeeze(), hwf.squeeze(), bound.squeeze()
 
-        tto_imgs, test_imgs = torch.split(imgs, [args.tto_views, args.test_views], dim=0)
-        tto_poses, test_poses = torch.split(poses, [args.tto_views, args.test_views], dim=0)
+        tto_imgs, test_imgs = torch.split(
+            imgs, [args.tto_views, args.test_views], dim=0)
+        tto_poses, test_poses = torch.split(
+            poses, [args.tto_views, args.test_views], dim=0)
 
         model.load_state_dict(meta_state)
         optim = torch.optim.SGD(model.parameters(), args.tto_lr)
 
         test_time_optimize(args, model, optim, tto_imgs, tto_poses, hwf, bound)
-        scene_psnr = report_result(args, model, test_imgs, test_poses, hwf, bound)
+        scene_psnr = report_result(
+            args, model, test_imgs, test_poses, hwf, bound)
 
         create_360_video(args, model, hwf, bound, device, idx+1, savedir)
-        
+
         print(f"scene {idx+1}, psnr:{scene_psnr:.3f}, video created")
         test_psnrs.append(scene_psnr)
-    
+
     test_psnrs = torch.stack(test_psnrs)
     print("----------------------------------")
     print(f"test dataset mean psnr: {test_psnrs.mean():.3f}")
+    wandb.log({'test/mean_psnr': test_psnrs.mean()})
 
 
 if __name__ == '__main__':
